b30a0eeaebfdccb8402ffcc1ea4a6c9ea08d8dea
Abhishek Williams <abhishekwilliams@Abhisheks-MacBook-Air.local>
2024-07-25 03:18:07 -0400

    Updated

--- blog_project/mysite/mysite/settings.py
+++ blog_project/mysite/mysite/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 ROOT_URLCONF = 'mysite.urls'
 
@@ -125,13 +126,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Use this folder to store collected static files
 
-
+# Additional static files (optional)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Custom static files
+]
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CRISPY_TEMPLATE_PACK="bootstrap4"
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
